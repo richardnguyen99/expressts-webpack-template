3484cf89be6f3a06362f55487cc24fab97bd8b2d
Richard Nguyen <richard@richardhnguyen.com>
2025-01-08 13:11:41 -0800

    refactor: refactor /archives routes

---
+++ src/routes/archive/month.ts
@@ -0,0 +1,45 @@
+import type { Request, Response } from "express-serve-static-core";
+
+import getArchives from "../../utils/archives";
+import { monthNames } from "../../utils/date";
+
+const archiveMonthHandler = async (req: Request, res: Response) => {
+  const { year, month: monthStr  } = req.params;
+  const archiveList = await getArchives();
+
+  if (!archiveList[year]) {
+    res.status(404);
+    res.send("Year not found");
+
+    return;
+  }
+
+  let month = Number(monthStr);
+  if (isNaN(Number(monthStr))) {
+    res.status(400);
+    res.send("Invalid month");
+  }
+
+  const posts = archiveList[year].posts.filter(
+    (post) =>
+      new Date(post.createdAt).getMonth() + 1 === month
+  );
+
+  if (posts.length === 0) {
+    res.status(404);
+    res.send("Month not found");
+
+    return;
+  }
+
+  res.render("archive", {
+    title: `Archives ${year}/${month}`,
+    page: "/archives",
+    env: process.env.NODE_ENV,
+    year,
+    month: monthNames[month - 1],
+    posts,
+  });
+};
+
+export default archiveMonthHandler;
---
+++ src/routes/archive/year.ts
@@ -0,0 +1,29 @@
+import type { Request, Response } from "express-serve-static-core";
+
+import getArchives from "../../utils/archives";
+
+const archiveYearHandler = async (req: Request, res: Response) => {
+  const { year } = req.params;
+  const archiveList = await getArchives();
+
+  if (!archiveList[year]) {
+    res.status(404);
+    res.send("Year not found");
+
+    return;
+  }
+
+  const posts = archiveList[year].posts.sort(
+    (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+  );
+
+  res.render("archive", {
+    title: `Archives ${year}`,
+    page: "/archives",
+    env: process.env.NODE_ENV,
+    year,
+    posts,
+  });
+}
+
+export default archiveYearHandler;
