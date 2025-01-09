a3f47464d60cb2c45e7fbdb1480bff7b59e72122
Richard Nguyen <richard@richardhnguyen.com>
2025-01-08 20:55:34 -0800

    feat: add unit tests for src/middlewares/requeset.mmiddleware.ts

---
+++ src/middlewares/__tests__/request.middleware.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import request from "supertest";
+
+import {
+  requestIdMiddleware
+} from "../request.middleware";
+
+describe("request middleware", () => {
+  let app: express.Application;
+
+  beforeAll(() => {
+    app = express();
+    app.use(requestIdMiddleware);
+  });
+
+  it("should add a unique request id to the response headers", (done) => {
+    app.get("/", (_req, res) => {
+      res.send("Hello World");
+    });
+
+    request(app)
+      .get("/")
+      .expect("X-Request-Id", /[a-f0-9-]{36}/)
+      .expect("Content-Length", "11")
+      .expect("Hello World")
+      .expect(200, done);
+  });
+});
+
