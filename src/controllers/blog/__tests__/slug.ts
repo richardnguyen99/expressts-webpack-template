17055d8848cde7b0a9f82987279609be06df46f5
Richard Nguyen <richard@richardhnguyen.com>
2025-01-15 13:56:49 -0800

    feat: add unit tests for blog comment  section

--- src/controllers/blog/__tests__/slug.ts
+++ src/controllers/blog/__tests__/slug.ts
@@ -161,6 +161,114 @@ describe("Blog Slug Controller", () => {
     });
   });
 
+  it("should render the comments section correctly", () => {
+    const $commentSection = $(".blog__comment-section");
+    expect($commentSection.length).toBe(1);
+
+    const $heading = $commentSection.find("h2");
+    expect($heading.length).toBeGreaterThan(0);
+    expect($heading.eq(0).text()).toBe("Comments");
+
+    const $comments = $commentSection.find(".blog__comments");
+    expect($comments.length).toBe(1);
+
+    const postComments = post?.comments || [];
+    const $commentItems = $comments.children();
+
+    expect($commentItems.length).toBe(postComments.length);
+    $commentItems.each((index, element) => {
+      const $commentItem = $(element);
+      const comment = postComments[index];
+
+      expect($commentItem.hasClass("comment-item")).toBe(true);
+      expect($commentItem.attr("data-comment-id")).toBe(comment.commentId);
+      expect($commentItem.attr("data-parent-url")).toBe(`/blogs/${post.slug}`);
+      expect($commentItem.get(0)?.tagName).toBe("div");
+
+      const $commentAvatar = $commentItem.find(".comment-item__avatar");
+      expect($commentAvatar.length).toBe(1);
+      expect($commentAvatar.get(0)?.tagName).toBe("div");
+
+      const $commentAvatarLink = $commentAvatar.find("a");
+      expect($commentAvatarLink.length).toBe(1);
+      expect($commentAvatarLink.attr("href")).toBe(
+        `/users/${comment.author?.userId}`,
+      );
+      expect($commentAvatarLink.attr("data-hover-card-url")).toBe(
+        `/users/${comment.author?.userId}/hover-card`,
+      );
+
+      const $commentAvatarImage = $commentAvatarLink.find("img");
+      expect($commentAvatarImage.length).toBe(1);
+      expect($commentAvatarImage.attr("src")).toBe(
+        comment.author?.profile.avatar,
+      );
+      expect($commentAvatarImage.attr("alt")).toBe(
+        comment.author?.profile.firstName +
+          " " +
+          comment.author?.profile.lastName,
+      );
+
+      const $commentContent = $commentItem.find(".comment-item__content");
+      expect($commentContent.length).toBe(1);
+
+      const $commentContainer = $commentContent.find(
+        ".comment-item__content__container",
+      );
+      expect($commentContainer.length).toBe(1);
+      expect($commentContainer.attr("id")).toBe(`comment-${comment.commentId}`);
+
+      const $commentHeader = $commentContainer.find(
+        ".comment-item__content__header",
+      );
+      expect($commentHeader.length).toBe(1);
+
+      const $commentAuthor = $commentHeader.find(
+        ".comment-item__content__header__author",
+      );
+      expect($commentAuthor.length).toBe(1);
+      expect($commentAuthor.get(0)?.tagName).toBe("a");
+      expect($commentAuthor.attr("href")).toBe(
+        `/users/${comment.author?.userId}`,
+      );
+      expect($commentAuthor.text()).toBe(
+        comment.author?.profile.firstName +
+          " " +
+          comment.author?.profile.lastName,
+      );
+
+      const $commentDateContainer = $commentHeader.find(
+        ".comment-item__content__header__date",
+      );
+      expect($commentDateContainer.length).toBe(1);
+
+      const $commentDate = $commentDateContainer.find("span");
+      expect($commentDate.text()).toBe(
+        new Intl.DateTimeFormat("en-US", {
+          dateStyle: "long",
+        }).format(new Date(comment.createdAt)),
+      );
+
+      const $commentTarget = $commentDateContainer.find("a");
+      expect($commentTarget.length).toBe(1);
+      expect($commentTarget.attr("href")).toBe(
+        `/blogs/${post.slug}#comment-${comment.commentId}`,
+      );
+      expect($commentTarget.attr("title")).toBe(
+        "Get permalink to this comment",
+      );
+      expect($commentTarget.find("i").hasClass("bi-hash")).toBe(true);
+
+      const $commentContentBody = $commentContainer.find(
+        ".comment-item__content__body",
+      );
+      expect($commentContentBody.length).toBe(1);
+
+      const $commentParagraph = $commentContentBody.find("p");
+      expect($commentParagraph.text()).toBe(comment.content);
+    });
+  });
+
   it("should return a 404 for an invalid blog post", async () => {
     expect(invalidResponse.status).toBe(404);
     expect(invalidResponse.text).toBe("Post not found");
