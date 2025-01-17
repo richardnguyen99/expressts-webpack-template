46ff6385faaa8ecb3ed43ccca3ed5093a4c218bd
Richard Nguyen <richard@richardhnguyen.com>
2025-01-17 15:20:05 -0800

    feat: add password toggle for visibility

--- src/static/js/form.js
+++ src/static/js/form.js
@@ -4,6 +4,7 @@ $(function () {
   const $forms = $("form");
 
   $forms.each(function () {
+    const $form = $(this);
     const $checkboxes = $(this).find("input[type=checkbox]");
 
     $checkboxes.each(function () {
@@ -18,5 +19,27 @@ $(function () {
         }
       });
     });
+
+    const $passwordVisibilityToggle = $(this).find("#passwordVisibilityToggle");
+
+    if ($passwordVisibilityToggle.length) {
+      const $btn = $passwordVisibilityToggle.find("button");
+      const $btnIcon = $btn.find("i");
+
+      $btn.on("click", function () {
+        const $passwordInput = $form.find(`#${$btn.data("target")}`);
+        console.log($btn.attr("aria-pressed"));
+
+        if ($btn.attr("aria-pressed") === "false") {
+          $btn.attr("aria-pressed", true);
+          $passwordInput.attr("type", "text");
+          $btnIcon.removeClass("bi-eye").addClass("bi-eye-slash");
+        } else {
+          $btn.attr("aria-pressed", false);
+          $passwordInput.attr("type", "password");
+          $btnIcon.removeClass("bi-eye-slash").addClass("bi-eye");
+        }
+      });
+    }
   });
 });
